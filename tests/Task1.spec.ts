67893be223ce143bcaf3e9076de98c9efeadee7d
jaredtrh <jaredtrh20@gmail.com>
2023-12-22 22:31:42 +0800

    why

--- tests/Task1.spec.ts
+++ tests/Task1.spec.ts
@@ -51,11 +51,12 @@ describe('Task1', () => {
     it('should', async () => {
         const claimer = await blockchain.treasury('claimer');
 
-        const claimResult = await task1.sendClaim(claimer.getSender(), toNano('0.05'));
+        const claimResult = await task1.sendClaim(claimer.getSender(), toNano('1000'));
         
         expect(claimResult.transactions).toHaveTransaction({
             from: task1.address,
             to: receiver.address,
+            value: undefined,
             success: true,
         });
 
